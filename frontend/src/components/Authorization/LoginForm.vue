b2571f27f666a4416046d4dbe2cc1bb4f4720c73
evgestrogan <evgestrogan@gmail.com>
2022-03-09 14:57:35 +0300

    [Frontend.Authorization.Login] - Реализовал форму входа в систему. [Frontend.UI.Input] - Создал компонент поля ввода текста. [Frontend.UI.Button] - Компонент кнопки. [Frontent] - Сделал логотип сервиса.

--- frontend/src/components/Authorization/LoginForm.vue
+++ frontend/src/components/Authorization/LoginForm.vue
@@ -1,8 +1,38 @@
 <template>
-  <h1>Login Form</h1>
+  <form class="mt-8 space-y-6" action="#" method="POST">
+    <input type="hidden" name="remember" value="true">
+    <div class="rounded-md shadow-sm -space-y-px">
+      <text-field
+        id="email-address"
+        name="email"
+        type="email"
+        autocomplete="email"
+        placeholder="Email"
+        required
+      ></text-field>
+      <text-field
+        id="password"
+        name="password"
+        type="password"
+        autocomplete="current-password"
+        placeholder="Пароль"
+        required
+      ></text-field>
+    </div>
+
+    <div class="flex items-center justify-end text-sm">
+      <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Забыли свой пароль?</a>
+    </div>
+
+    <div>
+      <btn type="submit">Войти</btn>
+    </div>
+  </form>
 </template>
 
 <script setup>
+import TextField from "../UI/Input/TextField.vue";
+import Btn from "../UI/Button/Btn.vue";
 </script>
 
 <style scoped>
---
+++ frontend/src/components/UI/Button/Btn.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :class="classes">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+import {reactive, computed} from 'vue'
+
+const listClasses = reactive([
+    'group',
+    'relative',
+    'w-full',
+    'flex',
+    'justify-center',
+    'py-2',
+    'px-4',
+    'border',
+    'border-transparent',
+    'text-sm',
+    'font-medium',
+    'rounded-md',
+    'text-white',
+    'bg-indigo-600',
+    'hover:bg-indigo-700',
+    'focus:outline-none',
+    'focus:ring-2',
+    'focus:ring-offset-2',
+    'focus:ring-indigo-500'
+]);
+
+const classes = computed(() => {
+  return listClasses.join(' ');
+});
+</script>
+
+<style scoped>
+
+</style>
