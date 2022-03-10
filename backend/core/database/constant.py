a0b62293a55d08bc7b6ae398c67a46b0ab9a4780
pushkin <maks.bur2809@gmail.com>
2022-03-10 12:32:35 +0300

    [backend.core.email] - создание драйвера для отправки email

--- backend/core/database/constant.py
+++ backend/core/database/constant.py
@@ -1,3 +1,5 @@
-USER = 'devFamilyTree'
-PASSWORD = '7Q9ynH3PAYejcNuc'
+import os
+
+USER = os.environ['mongo_user']
+PASSWORD = os.environ['mongo_password']
 CONNECTION_URL = f'mongodb+srv://{USER}:{PASSWORD}@cluster0.dkda1.mongodb.net/FamilyTree?retryWrites=true&w=majority'
---
+++ backend/core/email/constant.py
@@ -0,0 +1,4 @@
+import os
+
+MAIL_USER = os.environ['mail_user']
+MAIL_PASSWORD = os.environ['mail_password']
