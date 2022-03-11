9d9f2667e5dee5588a9233d1f9b47070d98eff7e
evgestrogan <evgestrogan@gmail.com>
2022-03-11 13:16:36 +0300

    [Backend.Authentication] - Реализация CRUD системы для таблицы пользователей V1.

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-from backend.login.urls import app_login
+from backend.authentication.urls import app_authentication
 from backend.tree.urls import app_tree
 
 app = FastAPI()
-app.mount('login', app_login)
-app.mount('tree', app_tree)
+app.mount('/authentication', app_authentication)
+app.mount('/tree', app_tree)
