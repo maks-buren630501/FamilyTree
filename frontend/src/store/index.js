d4623ff5ce61e54fb05b9b82f15468ff364fd9a6
evgestrogan <evgestrogan@gmail.com>
2022-03-22 15:25:50 +0300

    [Frontend.Notification] - сделал формы уведомлений.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex';
 import authorization from "./authorization";
+import notification from "./notification";
 
 export default createStore({
     modules: {
-        authorization
+        authorization,
+        notification
     }
 })
