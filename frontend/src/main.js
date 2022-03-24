c46475424a47c6ec5a90676e2b8a1a8bf0caccfa
evgestrogan <evgestrogan@gmail.com>
2022-03-24 13:14:51 +0300

    [Frontend.NavBar] - Сделал форму выподающего меню.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import store from './store';
 import router from "./router";
 import axios from './plugins/axios';
 import VueAxios from 'vue-axios';
+import vClickOutside from "click-outside-vue3";
 import './index.css';
 
-createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
+createApp(App).use(store).use(router).use(VueAxios, axios).use(vClickOutside).mount('#app')
